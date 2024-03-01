eada76eb5bd93e4d12ac80ddf9e738443bd04320
Roman Shelepov <shelepov-rmn@mail.ru>
2024-03-02 00:56:23 +0300

    Сделал задание

--- js/functions.js
+++ js/functions.js
@@ -18,7 +18,26 @@ const isPalindrome = (string) => {
 
 // 3. Функция, которая извлекает все цифры из строки и возвращаетих в виде целого пложительного числа.
 
+// 4. Выяснить, выходит ли встреча за рамки рабочего дня?
+
+const isLegit = (workingDayStart, workingDayEnd, meetingStart, meetingDuration) => {
+  const calcMinutes = (timeInHours) => {
+    const array = timeInHours.split(':');
+    return Number(array[0]) * 60 + Number(array[1]);
+  };
+
+  const workStartInMinutes = calcMinutes(workingDayStart);
+  const workEndInMinutes = calcMinutes(workingDayEnd);
+  const meetingStartInMinutes = calcMinutes(meetingStart);
+  const meetingEndInMinutes = meetingStartInMinutes + meetingDuration;
+
+  return meetingEndInMinutes <= workEndInMinutes && meetingStartInMinutes >= workStartInMinutes;
+};
+
+
 // Борьба с ESLint
 
 strLength('Строка', 5);
 isPalindrome('Строка');
+isLegit('08:00', '14:30', '14:00', 90);
+
